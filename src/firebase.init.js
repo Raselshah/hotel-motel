3131f00b9045d8c8c7a580b1f82d374fce0e6c03
Md Rasel Shah <mdraselshah56@gmail.com>
2022-04-13 20:42:40 +0600

    hide firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDM8qaD6HGMHqaWfGtkvFVcN0dzx8Br-6M",
-  authDomain: "hotel-motel-int.firebaseapp.com",
-  projectId: "hotel-motel-int",
-  storageBucket: "hotel-motel-int.appspot.com",
-  messagingSenderId: "992586308307",
-  appId: "1:992586308307:web:e9b5b80063847943d4d226",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
